12c5e600008ff9f78a8edf0f69e068d07ac93ad4
Alyona <alyona.derechey@gmail.com>
2022-07-15 05:32:24 +0100

    change Menu btn to Back btn in tutorials

--- openingschoosewindow.cpp
+++ openingschoosewindow.cpp
@@ -13,7 +13,7 @@ void OpeningsChooseWindow::on_pirc_def_button_clicked() {
   GameInfoDialog info(":/res/text/pirc_def.txt", this);
   info.exec();
   GameWindow win(true, new QString(":/res/text/pirc_def_fen.txt"),
-                 (QWidget *)QDialog::parent());
+                 (QWidget *)QDialog::parent(), 1);
   win.exec();
 }
 
@@ -21,7 +21,7 @@ void OpeningsChooseWindow::on_caro_kann_button_clicked() {
   GameInfoDialog info(":/res/text/caro_kann_def.txt", this);
   info.exec();
   GameWindow win(true, new QString(":/res/text/caro_kann_def_fen.txt"),
-                 (QWidget *)QDialog::parent());
+                 (QWidget *)QDialog::parent(), 1);
   win.exec();
 }
 
@@ -29,6 +29,6 @@ void OpeningsChooseWindow::on_italian_game_button_clicked() {
   GameInfoDialog info(":/res/text/italian_game.txt", this);
   info.exec();
   GameWindow win(true, new QString(":/res/text/italian_game_fen.txt"),
-                 (QWidget *)QDialog::parent());
+                 (QWidget *)QDialog::parent(), 1);
   win.exec();
 }
